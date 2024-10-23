ef087e4b7da2532e40b5165994f1bf7dd0a1893b
Linus Moreau <linus.moreau@gmail.com>
2024-10-23 13:25:41 -0700

    Fix jumping when releasing space bar

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -227,8 +227,8 @@ void WorldSystem::on_key(int key, int, int action, int mod)
                 break;
 		    case GLFW_KEY_SPACE:
                 // Dash
-                if (!player_comp.isJumping) {
-                    player_comp.isJumping = pressed;
+                if (!player_comp.isJumping && pressed) {
+                    player_comp.isJumping = true;
                     player_motion.velocity.z = 2;
                 }
                 break;

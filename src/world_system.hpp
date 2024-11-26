f312d7064c5b22ea150504614909d7166802bad8
Carlo Villaceran <cvillaceran@gmail.com>
2024-11-26 00:16:50 -0800

    add bomber spawn timer

--- src/world_system.hpp
+++ src/world_system.hpp
@@ -75,25 +75,24 @@ private:
 	};
 
 	const std::unordered_map<std::string, int> initial_max_entities = {
-		{"boar", 0},
-		{"barbarian", 0},
-		{"archer", 1},
-		{"bird", 0},
-		{"wizard", 0},
-		{"troll", 0},
-		{"bomber", 1},
+		{"boar", 1},
+		{"barbarian", 1},
+		{"archer", -1},
+		{"bird", 1},
+		{"wizard", -2},
+		{"troll", -3},
+		{"bomber", -2},
 		{"heart", 2},
 		{"collectible_trap", 2}
 	};
-
 	const std::unordered_map<std::string, float> initial_spawn_delays = {
-		{"boar", 9999999.0f},
-		{"barbarian", 9999999.0f},
-		{"archer", 9999999.0f},
-		{"bird", 9999999.0f},
-		{"wizard", 9999999.0f},
-		{"troll", 9999999.0f},
-		{"bomber", 9999999.0f},
+		{"boar", 10000.0f},
+		{"barbarian", 10000.0f},
+		{"archer", 20000.0f},
+		{"bird", 20000.0f},
+		{"wizard", 20000.0f},
+		{"troll", 30000.0f},
+		{"bomber", 20000.0f},
 		{"heart", 5000.0f},
 		{"collectible_trap", 5000.0f}
 	};
